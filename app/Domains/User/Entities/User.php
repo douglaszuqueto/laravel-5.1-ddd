5042cdd1db86551201e0fec1fca53b039c60e219
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2016-06-08 00:03:51 +0000

    Laravel DDD - Base

---
+++ app/Domains/User/Entities/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Domains\User\Entities;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+
+    protected $table = 'user';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Laravel 5.1 - DDD
